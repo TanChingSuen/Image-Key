72c46e46ad02781bd6d918a353dbdd308ce2a392
TanChingSuen <tanching.suen2021@gmail.com>
2022-01-28 16:07:45 -0500

    script file in html works now and connected to database in Node.js

--- server/index.js
+++ server/index.js
@@ -1,12 +1,32 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const mysql = require("mysql");
+const { rootCertificates } = require("tls");
 
 app.use(express.static(__dirname + "/../client"));
 
 app.get("/test", function (req, res) {
   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: null,
+  database: "test",
+});
+
+connection.connect();
+
+connection.query(
+  "SELECT password FROM `test1` WHERE ID = 1",
+  function (err, res, fields) {
+    console.log(res);
+  }
+);
+
+connection.end();
 
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
