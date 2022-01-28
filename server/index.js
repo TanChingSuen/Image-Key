56bf82a72e78903ea277413ebd19259e70c44d5c
TanChingSuen <tanching.suen2021@gmail.com>
2022-01-28 13:43:14 -0500

    2

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const path = require("path");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "../client/index.html"));
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
 app.listen(3000);
