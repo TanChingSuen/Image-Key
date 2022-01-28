bd052f7ec5d6a65a681a749469fffeba512c7c04
TanChingSuen <tanching.suen2021@gmail.com>
2022-01-28 13:48:49 -0500

    add login and register html

--- server/index.js
+++ server/index.js
@@ -6,4 +6,12 @@ app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
+app.get("/login", function (req, res) {
+  res.sendFile(path.join(__dirname, "../client/dist/login.html"));
+});
+
+app.get("/register", function (req, res) {
+  res.sendFile(path.join(__dirname, "../client/dist/register.html"));
+});
+
 app.listen(3000);
