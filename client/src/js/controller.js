aca9e6f77be528390fbbda50860271cd7a8c7f5c
TanChingSuen <tanching.suen2021@gmail.com>
2022-01-29 16:25:43 -0500

    start coding controller

---
+++ client/src/js/controller.js
@@ -0,0 +1,12 @@
+const login = document.querySelector(".nav__Login");
+const register = document.querySelector(".nav__Register");
+
+login.addEventListener("click", function (e) {
+  e.preventDefault();
+  console.log(1);
+});
+
+register.addEventListener("click", function (e) {
+  e.preventDefault();
+  console.log(1);
+});
