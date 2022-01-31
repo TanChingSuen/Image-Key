578257ea76346253fb7decd81171ee846f232baa
TanChingSuen <tanching.suen2021@gmail.com>
2022-01-31 16:54:13 -0500

    url change but the website isn't

--- client/src/js/controller.js
+++ client/src/js/controller.js
@@ -4,9 +4,11 @@ const register = document.querySelector(".register");
 login.addEventListener("click", function (e) {
   e.preventDefault();
   console.log(1);
+  window.history.pushState(null, "", "/login");
 });
 
 register.addEventListener("click", function (e) {
   e.preventDefault();
   console.log(1);
+  window.history.pushState(null, "", "/register");
 });
