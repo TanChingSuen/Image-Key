eaba6155b5191789ffdb223a5e533a7f74bf7fbb
TanChingSuen <tanching.suen2021@gmail.com>
2022-02-02 16:05:02 -0500

    now click the button will get u to that page at the front page

--- client/src/js/controller.js
+++ client/src/js/controller.js
@@ -5,10 +5,12 @@ login.addEventListener("click", function (e) {
   e.preventDefault();
   console.log(1);
   window.history.pushState(null, "", "/login");
+  location.reload();
 });
 
 register.addEventListener("click", function (e) {
   e.preventDefault();
   console.log(1);
   window.history.pushState(null, "", "/register");
+  location.reload();
 });
