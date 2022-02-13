a3d859e03571541fa263ed1a13887f8fbb93acc8
TanChingSuen <tanching.suen2021@gmail.com>
2022-02-13 14:29:54 -0500

    can take picture now

--- client/src/js/controller.js
+++ client/src/js/controller.js
@@ -1,16 +1,20 @@
 const login = document.querySelector(".login");
 const register = document.querySelector(".register");
+const face = document.querySelector(".face");
 
-login.addEventListener("click", function (e) {
-  e.preventDefault();
-  console.log(1);
-  window.history.pushState(null, "", "/login");
-  location.reload();
-});
-
-register.addEventListener("click", function (e) {
-  e.preventDefault();
-  console.log(1);
-  window.history.pushState(null, "", "/register");
-  location.reload();
-});
+if (login) {
+  login.addEventListener("click", function (e) {
+    e.preventDefault();
+    console.log(1);
+    window.history.pushState(null, "", "/login");
+    location.reload();
+  });
+}
+if (register) {
+  register.addEventListener("click", function (e) {
+    e.preventDefault();
+    console.log(1);
+    window.history.pushState(null, "", "/register");
+    location.reload();
+  });
+}
