64b2de03500682dcb84fe4be388b92d2471df796
TanChingSuen <tanching.suen2021@gmail.com>
2022-02-20 16:41:30 -0500

    Basically just missing the face recognition part

--- server/index.js
+++ server/index.js
@@ -13,6 +13,13 @@ const { connect } = require("http2");
 const b = require("based-blob");
 global.atob = require("atob");
 global.Blob = require("node-blob");
+const EventEmitter = require("events");
+const eventemitter = new EventEmitter();
+const faceapi = require("face-api.js");
+const BSON = require("bson");
+const mime = require("mime");
+const canvas = require("canvas");
+const tf = require("@tensorflow/tfjs-node");
 
 //Static file so I can use src from client file
 app.use(express.static(__dirname + "/../client"));
@@ -31,6 +38,17 @@ const loginStorage = multer.diskStorage({
 });
 const loginUpload = multer({ storage: loginStorage });
 
+//set up login face router
+const loginFaceStorage = multer.diskStorage({
+  dest: function (req, file, cb) {
+    cb(null, "./loginface/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const loginFaceUpload = multer({ storage: loginFaceStorage });
+
 //set up register image router
 const registerStorage = multer.diskStorage({
   dest: function (req, file, cb) {
@@ -42,36 +60,48 @@ const registerStorage = multer.diskStorage({
 });
 const registerUpload = multer({ storage: registerStorage });
 
-//Process login key image
+//set up register image router
+const registerFaceStorage = multer.diskStorage({
+  dest: function (req, file, cb) {
+    cb(null, "./registerface/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const registerFaceUpload = multer({ storage: registerFaceStorage });
 
+//Process login key image
 app
   .route("/loginimage")
   .post(loginUpload.single("image--key"), function (req, res, next) {
     let id;
     const data = fs.readFileSync(req.file.path, "base64");
-
     const connection = mysqlConn();
     connection.connect();
+    //Insert a account of fake ID of 9(10) with the input picture
     connection.query(
-      `SELECT ID from keyimageandface WHERE keyImage = '${blob}'`,
-      function (err, res) {
-        console.log(1, res);
+      `INSERT INTO keyimageandface (ID , keyImage) VALUES (2147483646 , '${data}')`
+    );
+    //Select ID with Picture of 9(10)
+    connection.query(
+      `SELECT ID FROM keyimageandface WHERE ID != 2147483646 and keyImage = (SELECT keyImage FROM keyimageandface WHERE ID = 2147483646)`,
+      (err, res) => {
+        if (err) throw err;
+        app.locals.loginID = res[0];
+        eventemitter.emit("loginImage");
       }
     );
-    connection.query(
-      `SELECT * from keyimageandface WHERE ID = 1`,
-      function (err, res) {
-        console.log(2, res);
-      }
-    );
+
+    //Delete 9(10) and its picture data
+    connection.query(`DELETE FROM keyimageandface WHERE ID = 2147483646`);
     connection.end();
-    app.locals.loginFindSame = 1;
     next();
   })
   .get(function (req, res) {
-    setTimeout(() => {
-      res.json({ findSame: app.locals.loginFindSame });
-    }, 5000);
+    eventemitter.on("loginImage", () => {
+      res.json(app.locals.loginID);
+    });
   });
 
 //Process register key image
@@ -80,13 +110,71 @@ app
   .post(registerUpload.single("r--image--key"), function (req, res, next) {
     console.log(req.file);
     const data = fs.readFileSync(req.file.path, "base64");
-    app.locals.regFindSame = 1;
+    const connection = mysqlConn();
+    connection.connect();
+    let newID;
+    connection.query(`SELECT MAX(ID) FROM keyimageandface`, (err, res) => {
+      if (err) throw err;
+      if (res) {
+        newID = res + 1;
+      }
+      console.log(newID);
+      eventemitter.emit("regImage");
+    });
+    connection.end();
   })
   .get(function (req, res) {
-    setTimeout(() => {
-      res.json({ findSame: app.locals.regFindSame });
-    }, 5000);
+    eventemitter.on("regImage", () => {
+      res.json(app.locals.regID);
+    });
   });
+
+//Process login face picture
+app
+  .route("/loginface")
+  .post(loginFaceUpload.single("login--face"), function (req, res, next) {
+    const facedata = req.body.loginface.split(",")[1];
+    const b64face = Buffer.from(facedata, "base64");
+    jimp.read(b64face, (err, res) => {
+      if (err) throw err;
+      res.quality(100).write("temp/facePic.png");
+    });
+    let dbface;
+    const connection = mysqlConn();
+    connection.connect();
+    connection.query(
+      `SELECT faceImage FROM keyimageandface WHERE ID = ${app.locals.loginID.ID}`,
+      (err, res) => {
+        if (err) throw err;
+        dbface = res;
+        eventemitter.emit("loginface");
+      }
+    );
+    connection.end();
+    eventemitter.on("loginface", () => {
+      dbface = JSON.stringify(dbface[0].faceImage);
+      const buf = Buffer.from(JSON.parse(dbface).data);
+      const dbface64 = buf.toString();
+      const dbfacebuf = Buffer.from(dbface64, "base64");
+      jimp.read(dbfacebuf, (err, res) => {
+        if (err) throw err;
+        res.quality(100).write("temp/dbface.png");
+        eventemitter.emit("emptyTemp");
+      });
+    });
+  });
+
+eventemitter.on("emptyTemp", () => {
+  fsExtra.emptyDirSync("./temp");
+});
+
+//Process register face picture
+app
+  .route("/registerface")
+  .post(
+    registerFaceUpload.single("register--face"),
+    function (req, res, next) {}
+  );
 
 function mysqlConn() {
   const connection = mysql.createConnection({
@@ -97,27 +185,6 @@ function mysqlConn() {
   });
   return connection;
 }
-
-/*
-//test of the mysql connection
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: null,
-  database: "test",
-});
-
-connection.connect();
-
-connection.query(
-  "SELECT password FROM `test1` WHERE ID = 1",
-  function (err, res, fields) {
-    console.log(res);
-  }
-);
-
-connection.end();
-*/
 
 //Main website
 app.get("/", function (req, res) {
