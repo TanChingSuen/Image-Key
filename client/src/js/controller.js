0fbef08c82710557813014585826417faa517652
TanChingSuen <tanching.suen2021@gmail.com>
2022-04-09 15:39:27 -0400

    Move script in html to controller

--- client/src/js/controller.js
+++ client/src/js/controller.js
@@ -4,6 +4,8 @@ const homepage = document.querySelector(".homepage");
 const face = document.querySelector(".face");
 const addbtn = document.querySelector(".add");
 const pwTable = document.querySelector(".pwTable");
+const loginPage = document.querySelector(".login-it");
+const registerPage = document.querySelector(".register-it");
 
 if (homepage) {
   homepage.addEventListener("click", function (e) {
@@ -24,6 +26,156 @@ if (register) {
     e.preventDefault();
     window.history.pushState(null, "", "/register");
     location.reload();
+  });
+}
+
+if (loginPage) {
+  function takeFacePic() {
+    const br = document.querySelector(".br");
+    br.insertAdjacentHTML(
+      "afterend",
+      `<div class="face">
+      <video id="webcam" autoplay playsinline width="640" height="480"></video>
+      <canvas id="canvas" class="d-none"></canvas>
+      <button class="facePic btn btn-primary">Take picture when you ready</button>
+    </div>`
+    );
+    if (document.querySelector(".non-face")) {
+      document.querySelector(".non-face").remove();
+    }
+    const webcamElement = document.getElementById("webcam");
+    const canvasElement = document.getElementById("canvas");
+    const faceBtn = document.querySelector(".facePic");
+    const webcam = new Webcam(webcamElement, "user", canvasElement, null);
+    webcam.start();
+    faceBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      const face = webcam.snap();
+      webcam.stop();
+      webcamElement.remove();
+      faceBtn.remove();
+      const formdata = new FormData();
+      formdata.append("loginface", face);
+      fetch("/loginface", {
+        method: "POST",
+        body: formdata,
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data === 3000) {
+            canvasElement.remove();
+            alert(
+              "We detect more than one face in the picture.\nPlease take another picture and make sure you are the only person in the picture"
+            );
+            takeFacePic();
+          } else if (data === 200) {
+            canvasElement.remove();
+            alert(
+              "We are not able to detect your face from the picture you just took.\nPlease take another picture"
+            );
+            takeFacePic();
+          } else if (data) {
+            alert("Login Success");
+            window.location.assign("/user");
+          } else {
+            canvasElement.remove();
+            localStorage.clear();
+            console.log(1);
+            alert(
+              "We are not able to match your face from the picture you just took with the picture in our database.\nPlease take another picture"
+            );
+            takeFacePic();
+          }
+        });
+    });
+  }
+  const form = document.querySelector(".key-image");
+  form.addEventListener("submit", (e) => {
+    let loginID;
+    fetch("/loginimage", {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        loginID = data.ID;
+        if (loginID) {
+          takeFacePic();
+        } else {
+          alert("This picture is not found, please submit the correct image.");
+        }
+      });
+  });
+}
+
+if (registerPage) {
+  function takeFacePic() {
+    const br = document.querySelector(".br");
+    br.insertAdjacentHTML(
+      "afterend",
+      `<div class="face">
+      <video id="webcam" autoplay playsinline width="640" height="480"></video>
+      <canvas id="canvas" class="d-none"></canvas>
+      <button class="facePic btn btn-primary">Take picture when you ready</button>
+    </div>`
+    );
+    if (document.querySelector(".non-face")) {
+      document.querySelector(".non-face").remove();
+    }
+    const webcamElement = document.getElementById("webcam");
+    const canvasElement = document.getElementById("canvas");
+    const faceBtn = document.querySelector(".facePic");
+    const webcam = new Webcam(webcamElement, "user", canvasElement, null);
+    webcam.start();
+    faceBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      const face = webcam.snap();
+      webcam.stop();
+      webcamElement.remove();
+      faceBtn.remove();
+      const formdata = new FormData();
+      formdata.append("registerface", face);
+      fetch("/registerface", {
+        method: "POST",
+        body: formdata,
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data === 3000) {
+            canvasElement.remove();
+            alert(
+              "We detect more than one face in the picture.\nPlease take another picture and make sure you are the only person in the picture"
+            );
+            takeFacePic();
+          } else if (data) {
+            alert("Register Success");
+            window.location.assign("/user");
+          } else {
+            canvasElement.remove();
+            alert(
+              "We are not able to detect your face from the picture you just took.\nPlease take another picture"
+            );
+            takeFacePic();
+          }
+        });
+    });
+  }
+  const form = document.querySelector(".key-image");
+  form.addEventListener("submit", (e) => {
+    let regID;
+    fetch("/registerimage", {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        regID = data;
+        if (regID) {
+          takeFacePic();
+        } else {
+          alert("This image is invalid, please submit another image");
+        }
+      });
   });
 }
 
